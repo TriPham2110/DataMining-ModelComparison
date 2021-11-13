f16bb4d899b836a14b5648589bcb0b5fe1de091d
TriPham2110 <thientri399@gmail.com>
2021-11-13 03:30:26 -0600

    Added data imputation

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer
 
 if __name__ == '__main__':
 
@@ -17,6 +21,28 @@ if __name__ == '__main__':
                               "Mitoses",
                               "Class"
                           ],
-                          sep=',')
+                          sep=',',
+                          na_values='?')
 
-    print(dataset)
+    dataset = dataset.drop("Sample_code_number", axis=1)
+
+    X = dataset.iloc[:, 0:-1]
+    y = dataset.iloc[:, -1]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
+
+    imputer = IterativeImputer()
+
+    # avoid data leakage (only fit the imputer with the training data)
+    imputer.fit(X_train)
+
+    # imputed X_train and X_test
+    X_train = imputer.transform(X_train)
+    X_test = imputer.transform(X_test)
+
+    # round the imputed values
+    X_train = X_train.round(0)
+    X_test = X_test.round(0)
+
+    print(X_test)
+    print(np.isnan(np.sum(X_test)))
